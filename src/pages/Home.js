3b075680a43bbb946ba009ec0e87521e605b72e0
Francisca <fraan928@gmail.com>
2022-01-04 06:44:39 -0300

    Getting the top ten recommended songs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Songs from "../components/Songs";
 import { SongContext } from "../contexts/SongContext";
+import { getTopSongs } from "../services/singMeASong";
 import { ContainerContent, ContainerPage } from "../styles/ContainerStyle";
 import { Button } from "../styles/Interactives";
 
 export default function Home() {
     const { currentSong } = useContext(SongContext);
 
-    const songs = [];
+    const [songs, setSongs] = useState([]);
 
+    useEffect(() => {
+        getTopSongs({ amount: 10 }).then((res) => setSongs(res.data)).catch((err) => console.error())
+    }, [])
 
     return (
         <ContainerPage>
--- src/services/singMeASong.js
+++ src/services/singMeASong.js
@@ -1,2 +1,11 @@
 import axios from "axios"
 import { api } from "./apiUrl"
+
+function getTopSongs({ amount }) {
+    const promise = axios.get(`${api}/recommendations/top/${amount}`);
+    return promise;
+}
+
+export {
+    getTopSongs,
+}
