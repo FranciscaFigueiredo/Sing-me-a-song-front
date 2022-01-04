6904892b09e2b460de5d47b0e41262f376f7e4cd
Francisca <fraan928@gmail.com>
2022-01-04 05:50:36 -0300

    footer layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import { SongContext } from "./contexts/SongContext";
 import GlobalStyle from "./styles/GlobalStyle";
 
 export default function App() {
+	const [currentSong, setCurrentSong] = useState(null);
+
 	return (
 		<BrowserRouter>
-			<GlobalStyle />
-		<Routes>
-			<Route path='/' element={<Home /> } />
-		</Routes>
+			<SongContext.Provider value={{ currentSong, setCurrentSong }} >
+				<GlobalStyle />
+				<Routes>
+					<Route path='/' element={<Home /> } />
+				</Routes>
+			</SongContext.Provider>
 		</BrowserRouter>
 	);
 }
