7f695ef9aa6e6b43d13ed00f0b3516251dda02dd
Francisca <fraan928@gmail.com>
2022-01-04 01:37:16 -0300

    Songs layout

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,13 +1,18 @@
+import SongCard from "./SongCard";
 import { SongsContainer } from "../styles/ContainerStyle";
 import { Line } from "../styles/TitleStyle";
 
-export default function Songs() {
+export default function Songs({ songs }) {
     return (
         <>
             <h2>Top músicas</h2>
             <Line />
             <SongsContainer>
-
+                {
+                    songs.length ?
+                        songs.map((song) => <SongCard song={song} />)
+                    :   <h3>Não há músicas registradas</h3>
+                }
             </SongsContainer>
         </>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import Songs from "../components/Songs";
 import { ContainerContent, ContainerPage } from "../styles/ContainerStyle";
 import { Button } from "../styles/Interactives";
 
 export default function Home() {
+    const [songs, setSongs] = useState([]);
+
+
     return (
         <ContainerPage>
             <Header />
             <ContainerContent>
                 <Button>Tocar aleatoriamente</Button>
                 <Button>Postar recomendações</Button>
-                <Songs />
+                <Songs songs={ songs } />
             </ContainerContent>
         </ContainerPage>
     );
